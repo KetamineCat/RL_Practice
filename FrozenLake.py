36c2b683ff650dcd463342d0769453b6abd330e4
KetamineCat <megatodenney@gmail.com>
2020-01-22 16:35:49 +0800

    Update FrozenLake.py

--- FrozenLake.py
+++ FrozenLake.py
@@ -7,30 +7,80 @@ env = gym.make("FrozenLake-v0") #from the gym's library
 action_size = env.action_space.n
 state_size = env.observation_space.n
 
-qtable = np.zeroes((state_size, action_size))
+qtable = numpy.zeroes((state_size, action_size))
 print(qtable)
 
-#Hyperparameters (
+#Parameters for each game and the learning rate/decay of the agent
 total_episodes = 10000
-learning_rate 0.8
-max_steps = 99 #Max steps nper episode
-gamma = 0.95 #Discounting rate (comes in at the gamma^n * R_(t+n+..))
+learning_rate = 0.8 #epsilon
+max_steps = 99 #Max steps per episode
+gamma = 0.95 #Discounting rate 
 
-#Exploration parameters
 epsilon = 0.1
 max_epsilon = 1.0
-min_epsilon = 0.01 #I wonder what the lowest possible epsilon value is, read gym sourcecode)
-decay_rate = 0.005 #Rate of change form exploration to exploitation over episodes
+min_epsilon = 0.01 #I wonder what the lowest possible epsilon value is i.e. I should RTFM
+decay_rate = 0.005 #Rate of change for the discounting rate
 
 #Reward list
 rewards = []
 
 #Rules for continuous learning in FrozenLake 
 for episode in range(total_episodes):
-    #Resets the environment (?)
+    #i.e. every episode is fresh
     step = 0
     done = False
     total_rewards = 0
     
+    for step in range(max_steps): 
+        exp_tradeoff = random.uniform(0,1)
+        
+        #defining exploitation
+        if exp_tradeoff > epsilon:
+        action = numpy.argmax(qtable[state,:])
+        
+        #defining exploration
+        else:
+        action = env.action_space.sample()
+    
+        new_state, reward, done, info = env.step(action) #RTFM OpenAI gym
+
+        #Using the Temporal Difference equation below
+        #Q(s,a):= Q(s,a) + learning_rate[reward + discounting_rate * Q_max(s,a) - Q(s,a)]
+        
+        #Weirdly enough epsilon is never explicitly defined in any of Simonini's texts and equations, so
+        #the relationship between the learning rate and the epsilon is another mystery to solve for me
+    
+        qtable[state,action] = qtable[state,action] + learning_rate * (reward + gamma * numpy.max(qtable[new_state, :]) - qtable[state,action])
+    
+        total_rewards += reward
+    
+        state = new_state # Placing this anywhere but near the bottom is most likely gonna break the code
+    
+        #Done = dead
+        if done == True:
+            break
+  
+print("Score over time: " + str(sum(rewards)/total_episodes))
+print(qtable)
+
+env.reset()
+
+for episode in range(5):
+    state = env.reset()
+    step = 0
+    done = Flase
+    print("FROZENLAKEFROZENLAKEFROZENLAKEFROZENLAKEFROZENLAKEFROZENLAKEFROZENLAKE")
+    print("EPISODE ", episode)
+    
+    #Exploit o'clock
     for step in range(max_steps):
-    [Continue from here]
+        action = numpy.argmax(qtable[state,:])
+        
+        new_state, reward, done, info = env.step(action)
+        
+        if done:
+            print("Number of steps: ", step)
+            break
+            
+        state = new_state
+env.close()
